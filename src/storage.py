62167b5e3e8e45223f0e18f085afb0e08073e66c
CzBiX <CzBiX@users.noreply.github.com>
2023-08-26 17:37:19 +0800

    use more level for storage path

--- src/storage.py
+++ src/storage.py
@@ -4,7 +4,10 @@ import os
 PREFIX = 'data/blobs'
 
 def get_file_object(vault_id: int, path_hash: str, readonly: bool = True):
-  path = os.path.join(PREFIX, str(vault_id), path_hash[:2], path_hash[2:])
+  path = os.path.join(
+    PREFIX, str(vault_id),
+    path_hash[:2], path_hash[2:4], path_hash[4:],
+  )
 
   if not readonly:
     os.makedirs(os.path.dirname(path), exist_ok=True)
