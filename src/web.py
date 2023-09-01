b87609ad7595456f3ff9365821b0454bb048e928
CzBiX <CzBiX@users.noreply.github.com>
2023-09-01 21:01:24 +0800

    refine cors support

--- src/web.py
+++ src/web.py
@@ -1,7 +1,6 @@
 import logging
 
-from fastapi import FastAPI, HTTPException
-from fastapi.middleware import cors
+from fastapi import FastAPI, HTTPException, Request, Response
 from fastapi.responses import JSONResponse
 
 from .routers import sync, subscription, user, vault
@@ -16,13 +15,6 @@ OBSIDIAN_APP_URLS = (
   'http://localhost',
 )
 
-app.add_middleware(
-  cors.CORSMiddleware,
-  allow_origins=OBSIDIAN_APP_URLS,
-  allow_methods=('*',),
-  allow_credentials=True,
-)
-
 @app.exception_handler(HTTPException)
 def catch_exceptions(_, exc: HTTPException):
   logger.warn('Exception caught', exc_info=exc)
@@ -32,6 +24,28 @@ def catch_exceptions(_, exc: HTTPException):
   }
   
   return JSONResponse(data)
+
+@app.middleware('http')
+async def add_cors_headers(request: Request, call_next):
+  if request.method == 'OPTIONS':
+    headers = {}
+
+    if origin := request.headers.get('origin'):
+      headers['Access-Control-Allow-Origin'] = origin
+
+    if method := request.headers.get('access-control-request-method'):
+      headers['Access-Control-Allow-Methods'] = method
+
+    if h := request.headers.get("access-control-request-headers"):
+      headers['Access-Control-Allow-Headers'] = h
+
+    return Response(status_code=204, headers=headers)
+  
+  response = await call_next(request)
+
+  response.headers['Access-Control-Allow-Origin'] = '*'
+  
+  return response
 
 app.include_router(sync.router, prefix='/sync')
 app.include_router(subscription.router, prefix='/subscription')
